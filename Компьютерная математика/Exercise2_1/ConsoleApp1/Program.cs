9300f51cecf5b142d90fdc0919632f49686f614f
Mita57 <57thrasher57@gmail.com>
2019-03-30 18:42:35 +0500

    ро

---
+++ Компьютерная математика/Exercise2_1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое множество");
+            char[] splitter = {' '};
+            var setA = Console.ReadLine().Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Введите второе множество");
+            var setB = Console.ReadLine().Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i<setA.Length; i++)
+            {
+                for(int j = 0; j<setB.Length; j++)
+                {
+                    Console.WriteLine(setA[i].ToString()+ "; "+ setB[j].ToString());
+                }
+            }
+        }
+    }
+}
