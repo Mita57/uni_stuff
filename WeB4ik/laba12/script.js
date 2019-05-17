d8a6aec1c0ae8b9dfcc9d91fe4bb8dbdc5b95f05
Timofey <48101066+Mita57@users.noreply.github.com>
2019-05-17 19:10:31 +0500

    lmxc

---
+++ WeB4ik/laba12/script.js
@@ -0,0 +1,4 @@
+function JS(){
+    $('#heading').text("JavaScript");
+    $('#info').text('often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web.[9] JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it,[10] and major web browsers have a dedicated JavaScript engine to execute it.');
+}
