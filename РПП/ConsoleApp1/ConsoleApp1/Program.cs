bbf243fb21e4090ed5398f73f816c3f9990435c3
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-11 21:01:03 +0500

    kkkj

---
+++ РПП/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+
+
+    class Circle
+    {
+        private int x;
+        private int y;
+        private int R;
+        private double S;
+
+        public string info()
+        {
+            return Convert.ToString(x+y+R+S);
+        }
+
+        public void simm()
+        {
+            this.x = -this.x;
+            this.y = -this.y;
+        }
+
+        public Circle(int x, int y, int R)
+        {
+            this.x = x;
+            this.y = y;
+            this.R = R;
+            getS();
+        }
+        public Circle()
+        {
+
+        }
+
+        private void setS()
+        {
+            this.S = Math.PI * (R * R);
+        }
+
+        public double getS()
+        {
+            return this.S;
+        }
+        private void setX(int x)
+        {
+            this.x = x;
+            setS();
+        }
+
+        private int getX()
+        {
+            return this.x;
+        }
+
+        private int getY()
+        {
+            return this.y;
+        }
+        
+        private void setY(int Y)
+        {
+            this.y = Y;
+            setS();
+        }
+
+        private void setR(int R)
+        {
+            this.R = R;
+            setS();
+        }
+
+        private int getR(int R)
+        {
+            return this.R;
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ РПП/dateTime/dateTime/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTime
+{
+    class myDate
+    {
+        private int day;
+        private int month;
+        private int year;
+        private DateTime fuckCS;
+
+        public myDate(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+            getDateTime();
+        }
+
+        private void getDateTime()
+        {
+            this.fuckCS = new DateTime(this.year, this.month, this.day);
+        }
+
+        public string showNormal()
+        {
+            return (this.day + "." + this.month + "." + this.year);
+        }
+
+        public string showFakinAmericano()
+        {
+            return (this.month + "." + this.day + "." + this.year);
+
+        }
+
+        public int countDifference(string date)
+        {
+            string[] parts = date.Split('.');
+            DateTime thisDate = new DateTime(int.Parse(parts[2]),int.Parse(parts[1]), int.Parse(parts[0]));
+            return (thisDate - fuckCS).Days;
+        }
+
+        public void setDay(int day)
+        {
+            this.day = day;
+            getDateTime();
+        }
+
+        public void setMonth(int month)
+        {
+            this.month = month;
+            getDateTime();
+        }
+
+        public void setYear(int year)
+        {
+            this.year = year;
+            getDateTime();
+        }
+
+        public void changeDays(int days)
+        {
+            this.fuckCS.AddDays(days);
+            this.day = fuckCS.Day;
+            this.month = fuckCS.Month;
+            this.year = fuckCS.Year;
+        }
+
+    }
+}
