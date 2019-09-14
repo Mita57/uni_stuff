57f51d6b957241f1346938db881d1fb64d8f1b03
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-15 01:00:15 +0500

    das

--- Year_2/СИАКОД/laba1/src/Main.java
+++ Year_2/СИАКОД/laba1/src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         }
         checkDuplicates(ara);
 
-        //
+        //mins sum of subarray
 
 
     }
@@ -107,22 +107,16 @@ public class Main {
     }
 
     private static String heightcheker(int[] ara) {
-        System.out.println("Введите количество работяг с завода");
-        Scanner sca = new Scanner(System.in);
-        int[] cock = new int[Integer.parseInt(sca.nextLine())];
-        for (int i = 0; i < cock.length; i++) {
-            cock[i] = (Integer.parseInt(sca.nextLine()));
-        }
         int temp = 0;
         int count = 0;
-        for (int i = 0; i < cock.length; i++) {
+        for (int i = 0; i < ara.length; i++) {
             boolean lul = false;
-            for (int j = 1; j < cock.length; j++) {
-                if (cock[j - 1] > cock[j]) {
+            for (int j = 1; j < ara.length; j++) {
+                if (ara[j - 1] > ara[j]) {
                     lul = true;
-                    temp = cock[j - 1];
-                    cock[j - 1] = cock[j];
-                    cock[j] = temp;
+                    temp = ara[j - 1];
+                    ara[j - 1] = ara[j];
+                    ara[j] = temp;
                 }
             }
             if(!lul){
