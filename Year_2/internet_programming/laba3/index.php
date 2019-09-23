f203a877041414b9fb960cece5b8027049f3e5b4
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-23 23:03:18 +0500

    k

---
+++ Year_2/internet_programming/laba3/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>gimme memes mf</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+<body>
+    <div class="inputs">
+        <form method="post" action="index.php">
+            <input type="text" name="rows" placeholder="Строки"><br>
+            <input type="text" name="cols" placeholder="Столбцы"><br>
+            <input type="text" name="min" placeholder="Минимум"><br>
+            <input type="text" name="max" placeholder="Максимум"><br>
+            <input type="submit" name="run" value="Let's go biach"><br>
+        </form>
+    </div>
+    <?php
+
+    $rows =  $_POST['rows'];
+    $cols = $_POST['cols'];
+    $ara = [];
+    if(isset($_POST['run'])){
+        for ($i = 0; $i < $rows; $i++){
+            for ($j = 0; $j < $cols; $j++){
+                $ara[$i][$j] = rand($_POST['min'], $_POST['max']);
+            }
+        }
+        $maxELemCol = 0;
+        $maxElem = $ara[$i][$j];
+        $minElemROw = 0;
+        $minElem = 0;
+        echo ("<div class='output'> Голодний: <br>");
+        for ($i = 0; $i < $rows; $i++){
+            for ($j = 0; $j < $cols; $j++){
+                echo ($ara[$i][$j].' '.' '. ' ');
+                if($ara[$i][$j]> $maxElem ){
+                    $maxElem = $ara[$i][$j];
+                    $maxELemCol = $j;
+                }
+                if($ara[$i][$j] < $minElem){
+                    $minElem = $ara[$i][$j];
+                    $minElemRow = $i;
+                }
+            }
+            echo ('<br>');
+        }
+        $araCock = $ara;
+        for ($i = 0; $i < $rows; $i++) {
+            $ara[$minElemROw][$i] = $araCock[$i][$maxELemCol];
+            $ara[$i][$maxELemCol] = $araCock[$maxELemCol][$i];
+        }
+        echo ("Бахнув пельменiв: <br>");
+        for ($i = 0; $i < $rows; $i++){
+            for ($j = 0; $j < $cols; $j++){
+                echo ($ara[$i][$j]).(' '.' '. ' ');
+            }
+            echo ("<br>");
+        }
+
+
+
+
+    }
+    ?>
+</body>
+</html>
