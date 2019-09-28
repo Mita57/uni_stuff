380f9ff7985de6085b5cbb0fcfb6423a318fcfc1
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-29 00:52:53 +0500

    asd

--- Year_2/internet_programming/laba4/index.php
+++ Year_2/internet_programming/laba4/index.php
@@ -16,7 +16,27 @@
         </form>
     </div>
     <?php
+    $SS = explode(" ",$_POST['cock']); //finally I've a var named SS in my project
+    for ($i = 0; $i < sizeof($SS); $i++){
+        if(!is_numeric($SS[$i])){
+            unset($SS[$i]);
+            continue;
+        }
+        else {
+            for ($j = 0; $j < strlen($SS[$i])-1; $i++) {
+                if(!($SS[$i][$j] <= $SS[$i][$j+1])){
+                    unset($SS[$i]);
+                    break;
+                }
+            }
+        }
+    }
 
+    echo ("<div class='outism'>") ;
+    for ($i = 0; $i < sizeof($SS); $i++){
+        echo ($SS[$i]." ");
+    }
+    echo ("</div>");
     ?>
 </body>
 </html>
