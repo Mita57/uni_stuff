51545d0dc3a02754010c8f6d80d9f6f0b3b3fe62
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-29 18:41:10 +0500

    ds

--- Year_2/ООП/oiok/inheritance/inheritance/Circle.cs
+++ Year_2/ООП/oiok/inheritance/inheritance/Circle.cs
@@ -41,9 +41,9 @@ namespace inheritance
         {
             return Math.PI * r * r;
         }
-        public new double ReFrom(Point a)
+        public  double ReFrom(Circle a)
         {
-            return (Math.Round(Math.Sqrt(Math.Abs(this.x - a.x) + Math.Abs(this.y - a.y)), 2))-r;
+            return (Math.Round(Math.Sqrt(Math.Abs(this.x - a.x) + Math.Abs(this.y - a.y)), 2))-this.r;
         }
     }
 }
