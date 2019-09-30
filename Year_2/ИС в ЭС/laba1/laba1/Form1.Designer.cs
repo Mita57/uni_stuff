91b632155f84163f556d601ce1fa463a732da17e
Timofey <48101066+Mita57@users.noreply.github.com>
2019-09-30 09:52:31 +0500

    fff

---
+++ Year_2/ИС в ЭС/laba1/laba1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountUpDown = new System.Windows.Forms.NumericUpDown();
+            this.inputDGW = new System.Windows.Forms.DataGridView();
+            this.autoDGW = new System.Windows.Forms.DataGridView();
+            this.manDGW = new System.Windows.Forms.DataGridView();
+            this.critIndex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.criName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.renderButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.amountUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDGW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoDGW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manDGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кол-во показаелей";
+            // 
+            // amountUpDown
+            // 
+            this.amountUpDown.Location = new System.Drawing.Point(122, 7);
+            this.amountUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.amountUpDown.Name = "amountUpDown";
+            this.amountUpDown.Size = new System.Drawing.Size(120, 20);
+            this.amountUpDown.TabIndex = 1;
+            this.amountUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // inputDGW
+            // 
+            this.inputDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inputDGW.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.critIndex,
+            this.criName});
+            this.inputDGW.Location = new System.Drawing.Point(12, 30);
+            this.inputDGW.Name = "inputDGW";
+            this.inputDGW.RowHeadersVisible = false;
+            this.inputDGW.Size = new System.Drawing.Size(379, 437);
+            this.inputDGW.TabIndex = 2;
+            // 
+            // autoDGW
+            // 
+            this.autoDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.autoDGW.Location = new System.Drawing.Point(421, 16);
+            this.autoDGW.Name = "autoDGW";
+            this.autoDGW.Size = new System.Drawing.Size(426, 236);
+            this.autoDGW.TabIndex = 3;
+            // 
+            // manDGW
+            // 
+            this.manDGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.manDGW.Location = new System.Drawing.Point(421, 261);
+            this.manDGW.Name = "manDGW";
+            this.manDGW.Size = new System.Drawing.Size(426, 236);
+            this.manDGW.TabIndex = 4;
+            // 
+            // critIndex
+            // 
+            this.critIndex.HeaderText = " ";
+            this.critIndex.Name = "critIndex";
+            this.critIndex.Width = 20;
+            // 
+            // criName
+            // 
+            this.criName.HeaderText = "Показатель";
+            this.criName.Name = "criName";
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(151, 474);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(75, 23);
+            this.executeButton.TabIndex = 5;
+            this.executeButton.Text = "Вычислить";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // renderButton
+            // 
+            this.renderButton.Location = new System.Drawing.Point(259, 4);
+            this.renderButton.Name = "renderButton";
+            this.renderButton.Size = new System.Drawing.Size(75, 23);
+            this.renderButton.TabIndex = 6;
+            this.renderButton.Text = "Render";
+            this.renderButton.UseVisualStyleBackColor = true;
+            this.renderButton.Click += new System.EventHandler(this.RenderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 505);
+            this.Controls.Add(this.renderButton);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.manDGW);
+            this.Controls.Add(this.autoDGW);
+            this.Controls.Add(this.inputDGW);
+            this.Controls.Add(this.amountUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.amountUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDGW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoDGW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manDGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown amountUpDown;
+        private System.Windows.Forms.DataGridView inputDGW;
+        private System.Windows.Forms.DataGridView autoDGW;
+        private System.Windows.Forms.DataGridView manDGW;
+        private System.Windows.Forms.DataGridViewTextBoxColumn critIndex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn criName;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Button renderButton;
+    }
+}
+
---
+++ Year_2/ИС в ЭС/laba1/laba1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void RenderButton_Click(object sender, EventArgs e)
+        {
+            inputDGW.Rows.Clear();
+            inputDGW.RowCount = Convert.ToInt32(amountUpDown.Value);
+            inputDGW.ColumnCount = Convert.ToInt32(amountUpDown.Value) + 2;
+            for (int i = 2; i < Convert.ToInt32(amountUpDown.Value) + 2; i++)
+            {
+                inputDGW.Columns[i].Width = 40;
+                inputDGW.Columns[i].HeaderText = (i - 1).ToString();
+                inputDGW.Rows[i - 2].Cells[0].Value = (i - 1).ToString();
+                inputDGW.Rows[i - 2].Cells[i].Value = 0;
+                inputDGW.Rows[i - 2].Cells[i].ReadOnly = true;
+            }
+            inputDGW.Columns.Add("lal", "ИБЗ");
+            inputDGW.Columns.Add("lalal", "Баллы 1");
+            inputDGW.Columns.Add("lalal", "Баллы 2");
+            inputDGW.Columns[Convert.ToInt32(amountUpDown.Value)+2].Width = 50;//ИБЗ
+            inputDGW.Columns[Convert.ToInt32(amountUpDown.Value)+3].Width = 50;//баллы1
+            inputDGW.Columns[Convert.ToInt32(amountUpDown.Value)+4].Width = 50;//баллы2
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            bool nullCheck = false;
+            for(int i = 0; i < Convert.ToInt32(amountUpDown.Value); i++)
+            {
+                if(inputDGW.Rows[i].Cells[1] == null)
+                {
+                    MessageBox.Show("Введите названия показателей");
+                    nullCheck = true;
+                    break;
+                }
+                if (inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 2].Value == null)
+                {
+                    MessageBox.Show("Проверьте индексы балло-значимости");
+                    nullCheck = true;
+                    break;
+                }
+                if(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 3].Value == null)
+                {
+                    MessageBox.Show("Проверьте баллы 1");
+                    nullCheck = true;
+                    break;
+                }
+                if (inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 4].Value == null)
+                {
+                    MessageBox.Show("Проверьте баллы 2");
+                    nullCheck = true;
+                    break;
+                }
+
+            }
+            //check if the IBZ is equal to 1
+            double cock = 0;
+            for(int i = 0; i < Convert.ToInt32(amountUpDown.Value); i++)
+            {
+                cock += Convert.ToDouble(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 2].Value);
+            }
+            if (cock != 1)
+            {
+                MessageBox.Show("Сумма ИБЗ должна быть равна 1");
+                nullCheck = true;
+            }
+
+            //check if the score 1 is max of 10
+
+            for (int i = 0; i < Convert.ToInt32(amountUpDown.Value); i++)
+            {
+                if(int.Parse(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 3].Value.ToString()) > 10 || int.Parse(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 3].Value.ToString()) < 0)
+                {
+                    MessageBox.Show("Проверьте баллы 1");
+                    nullCheck = true;
+                    break;
+                }
+            }
+            //check if the score 2 is max of 10
+
+            for (int i = 0; i < Convert.ToInt32(amountUpDown.Value); i++)
+            {
+                if (int.Parse(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 4].Value.ToString()) > 10 || int.Parse(inputDGW.Rows[i].Cells[Convert.ToInt32(amountUpDown.Value) + 4].Value.ToString()) < 0)
+                {
+                    MessageBox.Show("Проверьте баллы 1");
+                    nullCheck = true;
+                    break;
+                }
+            }
+
+        }
+
+        }
+    }
+    }
+
