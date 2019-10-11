a1edf8336787adacaaa0d261fe2140ba2c624dca
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-11 10:03:32 +0500

    dwdwqdwdwq

--- Year_2/Моделирование систем/laba1/laba1.2/laba1.2/Form1.cs
+++ Year_2/Моделирование систем/laba1/laba1.2/laba1.2/Form1.cs
@@ -12,27 +12,30 @@ namespace laba1._2
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            double n = Convert.ToInt32(lotsAmount.Text); //количество операторов n
-            double k = Convert.ToInt32(storageVolume.Text);//емкость накопителя, m
-            double a = Convert.ToDouble(streamDensity.Text);//плотность потока, Lambda
-            double t0 = Convert.ToDouble(averageTime.Text);//среднее обслуживание, Tao0
-            double u = 60 / t0; // поток обслуживаний, Myu, converted to hours
-            double pa = a / u;// плотность потока заявок, ro
+            double opsAmount = Convert.ToInt32(lotsAmount.Text); //количество операторов opsAmount
+            double bufferVolume = Convert.ToInt32(storageVolume.Text);//емкость накопителя, m
+            double strDensity = Convert.ToDouble(streamDensity.Text);//плотность потока, Lambda
+            double avgTime = Convert.ToDouble(averageTime.Text);//среднее обслуживание, Tao0
+            double serviceStream = 60 / avgTime; // поток обслуживаний, Myu, converted to hours
+            double requestsStreamDensity = strDensity / serviceStream;// плотность потока заявок, ro
             double p0 = 0;
-            for (int i = 0; i <= n; i++)
+            for (int i = 0; i <= opsAmount; i++)
             {
-                p0 += Math.Pow(pa, i) / factorial(i);
+                p0 += Math.Pow(requestsStreamDensity, i) / factorial(i);
             }
-            p0 += (Math.Pow(pa, n + 1) * (1 - Math.Pow(pa / n, k))) / (n * factorial(n) * (1 - pa / n));
-            p0 = 1/p0;
-            double potk = (Math.Pow(pa, n + k) / (Math.Pow(n, k) * factorial(n))) * p0; //среднее число отказов в обслуживании
-            deniedComplelely.Text = (Math.Round(potk, 3)*100).ToString();
-            double loch = (Math.Pow(pa, n + 1) * p0 * (1 - (k + 1 - k * (pa / n)) * Math.Pow(pa / n, k))) / (n * factorial(n) * Math.Pow((1 - pa / n), 2)); //среднее число заявок в очереди
-            double kk = pa * (1 - (Math.Pow(pa, n + k) / (Math.Pow(n, k) * factorial(n))) * p0); //среднее число занятых каналов
-            carsWaiting.Text = (Math.Round((loch / kk)*100, 3)).ToString();
-            avgBusyLots.Text = Math.Round(kk, 2).ToString();
-            avgBusyPed.Text = Math.Round(loch, 3).ToString();
-            eightHoursShit.Text = Math.Round((a * 8) * potk, 3).ToString();
+            p0 += (Math.Pow(requestsStreamDensity, opsAmount + 1) * (1 - Math.Pow(requestsStreamDensity / opsAmount, bufferVolume))) / (opsAmount * factorial(opsAmount) * (1 - requestsStreamDensity / opsAmount));
+            p0 = 1 / p0;
+            //среднее число отказов в обслуживании
+            double serviceDenies = (Math.Pow(requestsStreamDensity, opsAmount + bufferVolume) / (Math.Pow(opsAmount, bufferVolume) * factorial(opsAmount))) * p0;
+            deniedComplelely.Text = (Math.Round(serviceDenies, 3) * 100).ToString();
+            //среднее число заявок в очереди
+            double avgRequests = (Math.Pow(requestsStreamDensity, opsAmount + 1) * p0 * (1 - (bufferVolume + 1 - bufferVolume * (requestsStreamDensity / opsAmount)) * Math.Pow(requestsStreamDensity / opsAmount, bufferVolume))) / (opsAmount * factorial(opsAmount) * Math.Pow((1 - requestsStreamDensity / opsAmount), 2));
+            //среднее число занятых каналов
+            double avgBusy = requestsStreamDensity * (1 - (Math.Pow(requestsStreamDensity, opsAmount + bufferVolume) / (Math.Pow(opsAmount, bufferVolume) * factorial(opsAmount))) * p0);
+            carsWaiting.Text = (Math.Round((avgRequests / avgBusy) * 100, 3)).ToString();
+            avgBusyLots.Text = Math.Round(avgBusy, 2).ToString();
+            avgBusyPed.Text = Math.Round(avgRequests, 3).ToString();
+            eightHoursShit.Text = Math.Round((strDensity * 8) * serviceDenies, 3).ToString();
         }
 
 
