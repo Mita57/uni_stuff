66b05df5aa81d522367cfcca34ea7481e167ed98
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-15 23:08:19 +0500

    bj

---
+++ Year_2/internet_programming/laba7/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>UR MOM JUNIOR PHP PROGRAMMER</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+<body>
+
+    <div class="inputs">
+        <input type="date"name="date"><br>
+        <input type="text">
+    </div>
+
+
+    <?php
+    if(isset($_POST['run'])) {
+
+        $dirName = $_POST['dirName'];
+        $file1Name = $_POST['file1'];
+        $file2Name = $_POST['file2'];
+        $file3Name = $_POST['file3'];
+
+        $resultingFile = '';
+
+        //first file
+        $fileAd = $dirName . '/' . $file1Name;
+        $memes = fopen($fileAd, "r") or die("Unable to open file!");
+        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
+        //second file
+        $fileAd = $dirName . '/' . $file2Name;
+        $memes = fopen($fileAd, "r") or die("Unable to open file!");
+        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
+        //third file
+        $fileAd = $dirName . '/' . $file3Name;
+        $memes = fopen($fileAd, "r") or die("Unable to open file!");
+        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
+        //writing the final file
+        $fp = fopen($dirName . '/output.txt', "w");
+        fwrite($fp, $resultingFile);
+        fclose($fp);
+        echo(" <div class='outism'> Подкаталоги этого каталога: <br>");
+        $workFFS = scandir($dirName);
+        $workFFS = glob($dirName . '/*' , GLOB_ONLYDIR);
+        for($i = 0; $i < sizeof($workFFS); $i++){
+            echo(basename($workFFS[$i]) . " <br>");
+        }
+    }
+
+
+    ?>
+</body>
+</html>
