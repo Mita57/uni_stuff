87f052da6bae87cc2463256b0735b9aef7d2c5d4
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-16 00:28:29 +0500

    фвапифывиапи

--- Year_2/internet_programming/laba7/index.php
+++ Year_2/internet_programming/laba7/index.php
@@ -2,50 +2,44 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>UR MOM JUNIOR PHP PROGRAMMER</title>
+    <title>PI KRUTO</title>
     <link rel="stylesheet"  href="style.css">
 </head>
 <body>
 
     <div class="inputs">
-        <input type="date"name="date"><br>
-        <input type="text">
+        <form action="index.php" method="post">
+            <input type="date" name="date"><br>
+            <input type="text" name="stuff" placeholder="Write what u w0nt to find"><br>
+            <input type="submit" name="run" value="Let's do dis sh!t">
+        </form>
     </div>
 
 
     <?php
     if(isset($_POST['run'])) {
-
-        $dirName = $_POST['dirName'];
-        $file1Name = $_POST['file1'];
-        $file2Name = $_POST['file2'];
-        $file3Name = $_POST['file3'];
-
-        $resultingFile = '';
-
-        //first file
-        $fileAd = $dirName . '/' . $file1Name;
-        $memes = fopen($fileAd, "r") or die("Unable to open file!");
-        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
-        //second file
-        $fileAd = $dirName . '/' . $file2Name;
-        $memes = fopen($fileAd, "r") or die("Unable to open file!");
-        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
-        //third file
-        $fileAd = $dirName . '/' . $file3Name;
-        $memes = fopen($fileAd, "r") or die("Unable to open file!");
-        $resultingFile = $resultingFile . fread($memes, filesize($fileAd)) . "\n";
-        //writing the final file
-        $fp = fopen($dirName . '/output.txt', "w");
-        fwrite($fp, $resultingFile);
-        fclose($fp);
-        echo(" <div class='outism'> Подкаталоги этого каталога: <br>");
-        $workFFS = scandir($dirName);
-        $workFFS = glob($dirName . '/*' , GLOB_ONLYDIR);
-        for($i = 0; $i < sizeof($workFFS); $i++){
-            echo(basename($workFFS[$i]) . " <br>");
+        echo ('<div class="outism">');
+        $date = $_POST['date'];
+        $whatToLook = $_POST['stuff'];
+        $dirName = join(explode('-', $date));
+        $dirContains = scandir($dirName);
+        $foundAny = false;
+        for($i = 2; $i < sizeof($dirContains); $i++){
+            $fileAd = $dirName . '/' . $dirContains[$i];
+            $memes = fopen($fileAd, "r") or die("Unable to open file!");
+            $currFile = fread($memes, filesize($fileAd));
+            $currFileWords = explode(' ', $currFile);
+            if(in_array($whatToLook, $currFileWords)){
+                echo ($currFile);
+                $foundAny = true;
+            }
         }
+        if(!$foundAny){
+            echo ("We've nothing to show you :c");
+        }
+        echo ('</div>');
     }
+
 
 
     ?>
