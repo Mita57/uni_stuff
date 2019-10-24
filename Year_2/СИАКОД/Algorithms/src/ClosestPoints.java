ad2e9a02d934d23e06695b6615ccf7a80756a12a
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-24 23:55:37 +0500

    kj

---
+++ Year_2/СИАКОД/Algorithms/src/ClosestPoints.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ClosestPoints {
+    int[][] points;
+    int[][] kClosest(int[][] points, int K) {
+        this.points = points;
+        sort(0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private void sort(int i, int j, int K) {
+        if (i >= j) return;
+        int k = ThreadLocalRandom.current().nextInt(i, j);
+        swap(i, k);
+
+        int mid = partition(i, j);
+        int leftLength = mid - i + 1;
+        if (K < leftLength)
+            sort(i, mid - 1, K);
+        else if (K > leftLength)
+            sort(mid + 1, j, K - leftLength);
+    }
+
+    private int partition(int i, int j) {
+        int oi = i;
+        int pivot = dist(i);
+        i++;
+
+        while (true) {
+            while (i < j && dist(i) < pivot)
+                i++;
+            while (i <= j && dist(j) > pivot)
+                j--;
+            if (i >= j) break;
+            swap(i, j);
+        }
+        swap(oi, j);
+        return j;
+    }
+
+    private int dist(int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
+    }
+
+    private void swap(int i, int j) {
+        int t0 = points[i][0], t1 = points[i][1];
+        points[i][0] = points[j][0];
+        points[i][1] = points[j][1];
+        points[j][0] = t0;
+        points[j][1] = t1;
+    }
+    public static  void  main(String[] args){
+
+    }
+}
--- Year_2/СИАКОД/Algorithms/src/HuffmanNode.java
+++ Year_2/СИАКОД/Algorithms/src/HuffmanNode.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
-// node class is the basic structure
-// of each node present in the Huffman - tree.
+// node class is the basic structure of each node present in the Huffman - tree.
 class HuffmanNode {
 
     int data;
