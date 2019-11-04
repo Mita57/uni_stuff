55cb57350651b20f7f6713622e2fd1273c540539
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-04 18:40:37 +0500

    sdfgsdfdf

---
+++ Year_2/internet_programming/laba9/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>UR MOM JUNIOR PHP PROGRAMMER</title>
+    <link rel="stylesheet"  href="style.css">
+</head>
+<body>
+
+    <div class="inputs">
+        <form action="index.php" method="post">
+            <input type="text" placeholder="Имя каталога" name="dirName"><br>
+            <input type="text" placeholder="Файл 1" name="file1"><br>
+            <input type="text" placeholder="Файл 2" name="file2"><br>
+            <input type="text" placeholder="Файл 3" name="file3"><br>
+            <input type="submit" value="Погналити" name="run">
+        </form>
+    </div>
+
+
+    <?php
+
+
+    ?>
+</body>
+</html>
