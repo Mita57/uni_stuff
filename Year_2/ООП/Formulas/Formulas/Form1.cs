20b404ce2b9fe9ef718da8ea7f2fccf689496d54
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-10 17:31:52 +0500

    drfb

--- Year_2/ООП/Formulas/Formulas/Form1.cs
+++ Year_2/ООП/Formulas/Formulas/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Formulas
@@ -41,9 +35,35 @@ namespace Formulas
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            double velocity = (3 * 8.314 * double.Parse(temperature.Text)) / (double.Parse(textBox1.Text)*0.001);
-            velocity = Math.Sqrt(velocity);
-            textBox2.Text = Math.Round(velocity, 3).ToString();
+            string outism = "----------------------------------- \n";
+            outism += "%username% ";
+            outism += DateTime.Now.ToString("F");
+            outism += " \nИсходные данные: \n";
+            outism += "Масса молекулы: " + textBox1.Text;
+            outism += "\nТемпература: " + temperature.Text;
+            outism += "\nРезультаты \n";
+            if (double.Parse(textBox1.Text) <= 0)
+            {
+                outism += "Массас не может быть <= 0, лул \n";
+            }
+            if (double.Parse(temperature.Text) < 0)
+            {
+                outism += "Абсолютный нолб переоценен \n";
+            }
+
+            if (double.Parse(textBox1.Text) > 0 && double.Parse(temperature.Text) >= 0)
+            {
+                double velocity = (3 * 8.314 * double.Parse(temperature.Text)) / (double.Parse(textBox1.Text) * 0.001);
+                velocity = Math.Sqrt(velocity);
+                outism += "Среднеквадратичная скорость движения молекул: " + Math.Round(velocity, 3).ToString() + "\n";
+                textBox2.Text = Math.Round(velocity, 3).ToString();
+            }
+            else
+            {
+                textBox2.Text = "ЪЕЪ";
+                outism += "ЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕЪЕ\n";
+            }
+            System.IO.File.AppendAllText(@"C:\Users\57thr\Documents\GitHub\uni_stuff\Year_2\ООП\Formulas\Formulas\Log.txt", outism);
         }
 
 
@@ -52,11 +72,12 @@ namespace Formulas
         private void TextBox1_KeyDown(object sender, EventArgs e)
         {
             bool correct = double.TryParse(textBox1.Text, out double mass);
-            if (!correct || mass <= 0)
+            if (!correct)
             {
+                int pos = textBox1.SelectionStart - 1;
                 textBox1.Text = lastMassCorrect;
                 textBox1.Focus();
-                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.SelectionStart = pos;
                 textBox1.SelectionLength = 0;
             }
             else
@@ -72,11 +93,12 @@ namespace Formulas
         private void Temperature_KeyUp(object sender, EventArgs e)
         {
             bool correct = double.TryParse(temperature.Text, out double temp);
-            if (!correct || temp < 0)
+            if (!correct)
             {
+                int pos = temperature.SelectionStart - 1;
                 temperature.Text = lastTempCorrect;
                 temperature.Focus();
-                temperature.SelectionStart = temperature.Text.Length;
+                temperature.SelectionStart = pos;
                 temperature.SelectionLength = 0;
             }
             else
