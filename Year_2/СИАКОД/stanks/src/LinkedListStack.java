10242dbe2286d26e8f3339def826390f7426ee35
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-14 15:58:07 +0500

    zsxdf

--- Year_2/СИАКОД/stanks/src/LinkedListStack.java
+++ Year_2/СИАКОД/stanks/src/LinkedListStack.java
@@ -1,45 +1,71 @@
 import java.util.LinkedList;
 
-public class LinkedListStack <E>{
-    int size;
-    StackNode head;
+public class LinkedListStack <E> {
+    private int size;
+    private StackNode head;
 
     public void pop(){
-
+        this.head = head.getNext();
+        this.size--;
     }
 
-    public void push(){
-
+    public void push(StackNode node) {
+        StackNode temp = this.head;
+        this.head = node;
+        this.head.next = temp;
+        size++;
     }
+
+    public E peek() {
+        return (E) this.head.value;
+    }
+
+    public void print() {
+        StackNode node = this.head;
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+
 
     public int getSize(){
         return this.size;
     }
 
-    public LinkedListStack(StackNode head){
+    public LinkedListStack(StackNode head) {
         this.head = head;
         int size = 0;
         StackNode node = head;
         while(node != null){
-            node  = node.getNext();
+            node = node.getNext();
             size++;
         }
     }
-
-
-
-
 
     private static class StackNode<E>{
         E value;
         StackNode next;
 
+        public StackNode(E value, StackNode next) {
+            this.value = value;
+            this.next = next;
+        }
+
         public E getValue(){
             return this.value;
         }
 
+        public void setValue(E value){
+            this.value = value;
+        }
+
         public StackNode getNext() {
             return next;
+        }
+        public void setNext(StackNode next){
+            this.next = next;
         }
 
 
