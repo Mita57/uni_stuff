1513ace361360c089cdb4142b5722b07e1fb09f3
Timofey <48101066+Mita57@users.noreply.github.com>
2019-12-18 00:27:03 +0500

    asdafasd

---
+++ Year_2/СИАКОД/sort_algorithms/src/SortingAlgorithms.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+
+public class SortingAlgorithms<E extends Number, priavte> {
+    public void quickSort(E[] array, int low, int high) {
+        if (low < high) {
+
+            int parted = partition(array, low, high);
+
+            quickSort(array, low, parted - 1);
+            quickSort(array, parted + 1, high);
+        }
+    }
+
+    private int partition(E[] arr, int low, int high) {
+        E pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if ((double) arr[j] < (double) pivot) {
+                i++;
+                E temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        E temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+
+    public void mergeSort(E[] array, int i, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(array, l, l, m);
+            mergeSort(array, l, m + 1, r);
+            mergeSort(array, l, m, r);
+        }
+    }
+
+    private void merge(E[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        ArrayList<E> L = new ArrayList<E>();
+        ArrayList<E> R = new ArrayList<E>();
+        for (int i = 0; i < n1; i++) {
+            L.add(arr[l + i]);
+        }
+        for (int j = 0; j < n2; j++) {
+            R.add(arr[m + 1 + j]);
+        }
+        int i = 0;
+        int j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if ((double) L.get(i) <= (double) R.get(j)) {
+                arr[k] = L.get(i);
+                i++;
+            } else {
+                arr[k] = R.get(j);
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L.get(i);
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R.get(j);
+            j++;
+            k++;
+        }
+    }
+
+
+    public void bogoSortLul(E[] array) {
+        while (!isSorted(array)) {
+            shuffle(array);
+        }
+    }
+
+    private void shuffle(E[] array) {
+        for (int i = 1; i <= array.length - 1; i++) {
+            swap(array, i, (int) (Math.random() * i));
+        }
+    }
+
+    private boolean isSorted(E[] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if ((double) array[i] < (double) array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    void swap(E[] array, int i, int j) {
+        E temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- Year_2/СИАКОД/trees/src/AVL.java
+++ Year_2/СИАКОД/trees/src/AVL.java
@@ -1,109 +1,194 @@
-public class AVL {
-    private AVLNode head;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class AVL<E extends Number> {
+    private AVLNode<E> head;
     private int height;
 
-    public AVLNode getHead() {
+    public AVLNode<E> getHead() {
         return this.head;
     }
 
-    public void setHead(AVLNode value) {
+    public void setHead(AVLNode<E> value) {
         this.head = value;
     }
 
-    public int getHeight() {
-        return this.height;
+    private int height(AVLNode<E> node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
     }
 
-    private void setHeight() {
-
+    private int balance(AVLNode<E> node) {
+        if(node == null){
+            return 0;
+        }
+        return (height(node.left) - height(node.right));
     }
 
-    public AVL(AVLNode head) {
+
+    public AVL(AVLNode<E> head) {
         this.head = head;
-        setHeight();
     }
 
-    public addElement(AVLNode element) {
-
+    private AVLNode<E> rightRotate(AVLNode<E> y) {
+        AVLNode<E> x = y.left;
+        AVLNode<E> T2 = x.right;
+        x.right = y;
+        y.left = T2;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        return x;
     }
 
-    public Number findElement(Number value) {
-
+    private AVLNode<E> leftRotate(AVLNode<E> x) {
+        AVLNode<E> y = x.right;
+        AVLNode<E> T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        return y;
     }
 
-    public void printTree() {
+    public AVLNode<E> insert(AVLNode<E> node, E value) {
+        if(node == null) {
+            return (new AVLNode<E>(value));
+        }
+        if((double)value < (double)node.value) {
+            node.left = insert(node.left, value);
+        }
+        else{
+            if((double)value > (double)node.value) {
+                node.right = insert(node.right, value);
+            }
+            else {
+                return node;
+            }
+        }
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        int balance = balance(node);
+        if (balance > 1 && (double)value < (double)node.left.value) {
+            return rightRotate(node);
+        }
+        if (balance < -1 && (double)value > (double)node.right.value) {
+            return leftRotate(node);
+        }
+        if (balance > 1 && (double)value > (double)node.left.value) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && (double)value < (double)node.right.value) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
 
+    public AVLNode<E> findElement(AVLNode<E> node, E value) {
+        if((double)value < (double)node.value) {
+            findElement(node.left, value);
+        }
+        else{
+            if((double)value > (double)node.value) {
+                findElement(node.right, value);
+            }
+            else {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    public void printTree(AVLNode<E> node) {
+        if (node != null) {
+            System.out.print(node.value + ", ");
+            printTree(node.left);
+            printTree(node.right);
+        }
+    }
+
+    private int elementsAmount = 0;
+    private double elementsSum = 0;
+    private ArrayList<E> elements = new ArrayList<>();
+
+    private void preOrder(AVLNode<E> node) {
+        if (node != null) {
+            elementsSum += (double)node.value;
+            elementsAmount++;
+            elements.add(node.value);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public double standartDeviation() {
+        double avg = elementsSum / elementsAmount;
+        double sum = 0;
+        for(E X: elements) {
+            sum += ((double)X - avg) * ((double)X - avg);
+        }
+        return Math.sqrt(sum / elementsAmount);
+    }
+
+    public void postOrderTraverse() {
+        AVLNode<E> node = this.head;
+        Stack<AVLNode<E>> s1 = new Stack<AVLNode<E>>();
+        Stack<AVLNode<E>> s2 = new Stack<AVLNode<E>>();
+        s1.push(node);
+        while (!s1.isEmpty()) {
+            AVLNode<E> temp = s1.pop();
+            s2.push(temp);
+            if(temp.left!=null){
+                s1.push(temp.left);
+            }
+            if(temp.right!=null){
+                s1.push(temp.right);
+            }
+        }
+        System.out.println("Postorder Traversal: ");
+        while(!s2.isEmpty()){
+            System.out.print(s2.pop());
+        }
     }
 
 
 
-    class AVLNode<Number> {
-        private AVLNode left;
-        private int leftBalance;
-        private int rightBalance;
-        private AVLNode right;
-        private Number value;
 
-        public AVLNode getLeft() {
+
+
+    class AVLNode<E extends Number> {
+        private AVLNode<E> left;
+        private AVLNode<E> right;
+        private int height;
+        private E value;
+
+        public AVLNode<E> getLeft() {
             return this.left;
         }
-
-        public void setLeft(AVLNode node) {
-            this.left = node;
+        public int getHeight(){
+            return this.height;
         }
 
-        public AVLNode getRight() {
-            return this.right;
-        }
 
-        public void setRight(AVLNode node) {
-            this.right = node;
-        }
-
-        public Number getValue() {
+        public E getValue() {
             return this.value;
         }
 
-        public void setValue(Number value) {
+        public void setValue(E value) {
             this.value = value;
         }
 
-        public int getLeftBalance() {
-            return this.leftBalance;
-        }
 
-        public int getRightBalance() {
-            return this.rightBalance;
-        }
-
-
-        private void setLeftBalance() {
-            int counter = 0;
-            AVLNode node = head;
-            while(node != null){
-                counter++;
-                node = node.getRight();
-            }
-        }
-
-        private void setRightBalance() {
-
-        }
-
-        private int countBalance() {
-            return this.rightBalance - this.leftBalance;
-        }
-
-
-
-
-
-        public AVLNode(AVLNode left, AVLNode right, Number value){
-            this.left = left;
-            this.right = right;
+        public AVLNode(E value){
             this.value = value;
+            this.height = 1;
+
         }
+
     }
+
 
 }
 
