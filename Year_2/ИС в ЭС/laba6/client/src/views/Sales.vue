f2ad3eaac67cd03ec288d306a04c8928b13cf501
Timofey <48101066+Mita57@users.noreply.github.com>
2020-01-10 18:42:56 +0500

    ssss

--- Year_2/ИС в ЭС/laba6/client/src/views/Sales.vue
+++ Year_2/ИС в ЭС/laba6/client/src/views/Sales.vue
@@ -1,1 +1,40 @@
-//TODO: wtf is this?
+<template>
+    <div class="ma-10 mt-4">
+        <v-date-picker v-model="picker" landscape type="month"></v-date-picker> <br>
+        <v-date-picker v-model="picker" landscape class="mt-5" type="month"></v-date-picker>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "Waybills",
+        data: () => ({}),
+
+        computed: {
+            formTitle() {
+                return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+            },
+        },
+
+        watch: {
+            dialog(val) {
+                val || this.close()
+            },
+        },
+
+        created() {
+            this.initialize()
+        },
+
+        methods: {
+            initialize() {
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
