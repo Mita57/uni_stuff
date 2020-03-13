c8f9cea1cc88b893d74923bdc4260913f50cce11
Timofey <48101066+Mita57@users.noreply.github.com>
2020-03-14 00:35:21 +0500

    wfwffw

---
+++ Year_2/Peace sells... But who's buying/fefefdfdfs/Rational.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incapsulation.RationalNumbers
+{
+    public class Rational
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int Numerator
+        {
+            get => _numerator;
+            set => _numerator = value;
+        }
+
+        public int Denominator
+        {
+            get => _denominator;
+            set => _denominator = value;
+        }
+
+        public Rational(int numenator, int denominator = 0)
+        {
+            this.Numerator = numenator;
+            this.Denominator = denominator;
+        }
+
+        public bool IsNan
+        {
+            get
+            {
+                if (this._denominator == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static Rational operator +(Rational a, Rational b)
+        {
+            int newNumerator = a.Numerator + b.Numerator;
+            return new Rational(newNumerator, b.Denominator);
+        }
+
+        public static Rational operator -(Rational a, Rational b)
+        {
+            int newNumerator = a.Numerator - b.Numerator;
+            return new Rational(newNumerator, b.Denominator);
+        }
+
+        public static Rational operator *(Rational a, Rational b)
+        {
+            int newNumerator = a.Numerator * b.Numerator;
+            int newDenominator = a.Denominator * b.Denominator;
+            return new Rational(newNumerator, newDenominator);
+        }
+
+        public static Rational operator /(Rational a, Rational b)
+        {
+            int newNumerator = a.Numerator * b.Denominator;
+            int newDenominator = a.Denominator * b.Numerator;
+            return new Rational(newNumerator, newDenominator);
+        }
+
+        public static implicit operator double(Rational a)
+        {
+            return a.Numerator / a.Denominator;
+        }
+
+        public static implicit operator Rational(int a)
+        {
+            return new Rational(a);
+        }
+    }
+}
