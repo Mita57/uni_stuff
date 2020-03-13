615ec70ec80ed308eaae876e7624eac076f3e3aa
Timofey <48101066+Mita57@users.noreply.github.com>
2020-03-13 11:42:01 +0500

    dflf

--- Year_2/БИПИП/laba3/laba3/laba3/Form1.cs
+++ Year_2/БИПИП/laba3/laba3/laba3/Form1.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,18 +19,22 @@ namespace laba3
             InitializeComponent();
         }
 
+        static int parts = 0;
+        static int high = 0;
+
         static double segmentation = 0;
         
         private void button1_Click(object sender, EventArgs e)
         {
-            int iterations = (int)iterationsAmount.Value + 1;
+            int iterations = (int)iterationsAmount.Value;
+            int threads = (int)threadsAmount.Value;
             double result = 0;
-            segmentation = (double)1 / iterations;
-
             double[] args = new double[iterations];
             double[] vals = new double[iterations];
-
+            segmentation = 1/(double)iterations;
+            parts = iterations / threads;
             args[0] = 1;
+            high = 0;
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -43,16 +48,28 @@ namespace laba3
                 vals[i] = fun(args[i]);
             }
 
+
             if(rectangleRB.Checked)
             {
-                result = rectangleMethod(vals);
+                for (int i = 0; i < threads; i++)
+                {
+                    Thread myNewTread = new Thread(() => result += rectangleMethod(vals, high, high + parts));
+                    myNewTread.Start();
+                }
             }
             else
             {
-                result = trapeezeMethod(vals);
+                for (int i = 0; i < threads; i++)
+                {
+                    Thread myNewTread = new Thread(() => result += trapeezeMethod(vals, high, high + parts));
+                    myNewTread.Start();
+                }
             }
 
             sw.Stop();
+
+            string final = "Результат: " + result + "\nВремя выполнения: " + sw.ElapsedMilliseconds;
+            richTextBox1.Text = final;
         }
         
         static double trapeezeMethod(double[] vals, int begin, int end)
@@ -62,16 +79,18 @@ namespace laba3
             {
                 value += ((vals[i - 1] + vals[i]) / 2) * segmentation;
             }
+            high += parts;
             return value;
         }
 
-        static double rectangleMethod(double[] vals)
+        static double rectangleMethod(double[] vals, int begin, int end)
         {
             double value = 0;
-            for (int i = 0; i < vals.Length; i++)
+            for (int i = begin; i < end; i++)
             {
                 value += vals[i] * segmentation;
             }
+            high += parts;
             return value;
         }
 
