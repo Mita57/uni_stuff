16f2a5772e899de74ee25ac7a25f65a45109bb54
Timofey <48101066+Mita57@users.noreply.github.com>
2020-04-10 23:39:07 +0500

    wefv

---
+++ Year_2/ООП/semester2/laba12/laba12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+
+namespace laba12
+{
+    abstract class Person
+    {
+        public string Name { get; private set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public abstract string Info();
+    }
+
+    class Stud : Person
+    {
+        string Spec { get; set; }
+        private string Group { get; set; }
+
+        public Stud(string nam, string sp, string gr) : base(nam)
+        {
+            Spec = sp;
+            Group = gr;
+        }
+
+        public override string Info()
+        {
+            return string.Format("Студент: {0}, специальность: \"{1}\", группа {2}", Name, Spec, Group);
+        }
+    }
+
+    class Prof : Person
+    {
+        private string Dep { get; set; }
+
+        public Prof(string nam, string dp) : base(nam)
+        {
+            Dep = dp;
+        }
+
+        public override string Info()
+        {
+            return String.Format("Преподователь:{0}, кафедра \"{1}\"", Name, Dep);
+        }
+    }
+
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- Year_2/ООП/semester2/laba12/laba12/Properties/AssemblyInfo.cs
+++ Year_2/ООП/semester2/laba12/laba12/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("laba12")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("laba12")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("BBDF13B2-74EC-4CB3-9A3D-BDEF5EB60AB8")]
+[assembly: Guid("ED84FD0C-BA9C-4464-8DA2-3770D9A5108E")]
 
 // Version information for an assembly consists of the following four values:
 //
