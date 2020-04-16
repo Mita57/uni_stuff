fad9338a1945f07456a03e2f5d83ac9c8d04af46
Timofey <48101066+Mita57@users.noreply.github.com>
2020-04-17 00:02:06 +0500

    wdq

--- Year_2/ООП/semester2/laba13/laba13/Program.cs
+++ Year_2/ООП/semester2/laba13/laba13/Program.cs
@@ -1,7 +1,87 @@
-﻿namespace laba13
+﻿using System;
+
+namespace laba13
 {
     internal class Program
     {
+        abstract class Figure
+        {
+            public int X
+            {
+                get { return X; }
+
+                set
+                {
+                    if (Math.Abs(value) > 8)
+                    {
+                        X = value;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            public int Y
+            {
+                get { return Y; }
+
+                set
+                {
+                    if (Math.Abs(value) > 8)
+                    {
+                        Y = value;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            public double S;
+
+            public void MoveTo(int x, int y)
+            {
+                if (Math.Abs(x) > 8 || Math.Abs(y) > 8)
+                {
+                    return;
+                }
+                else
+                {
+                    this.X = x;
+                    this.Y = y;
+                }
+            }
+
+            public void MoveRef(int dX, int dY)
+            {
+                if (Math.Abs(X + dX) > 8 || Math.Abs(Y + dY) > 8)
+                {
+                    return;
+                }
+                else
+                {
+                    this.X += dX;
+                    this.Y += dY;
+                }
+            }
+
+            public bool Equal(Figure fig)
+            {
+                if (this.S == fig.S && this.GetType().Name == fig.GetType().Name)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+        
+        
+
+
         public static void Main(string[] args)
         {
         }
