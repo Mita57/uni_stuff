3980f9fb3b7b5c29a4b814b37b490686eb76bfc7
Mita57 <57thrasher57@gmail.com>
2020-04-26 17:41:47 +0500

    qwd

--- Year_2/ООП/semester2/laba14/laba14/Program.cs
+++ Year_2/ООП/semester2/laba14/laba14/Program.cs
@@ -19,7 +19,7 @@ namespace laba14
             Spec = aSpec;
             Course = aCourse;
             Surname = aSurname;
-            Papa = a00Papa;
+            Papa = aPapa;
         }
     }
 
@@ -29,6 +29,65 @@ namespace laba14
 
         public delegate bool LessRule(Student S1, Student S2);
 
+        public delegate string PrintRule();
+
+        public static string full()
+        {
+            string outism = "";
+            foreach (var stud in Studs)
+            {
+                outism += (stud as Student).Surname + "; ";
+                outism += (stud as Student).Name + "; ";
+                outism += (stud as Student).Papa + "; Спец.: ";
+                outism += (stud as Student).Spec + "; Курс: ";
+                outism += (stud as Student).Course + "\n";
+            }
+            
+            return outism;
+        }
+
+        public static string brief()
+        {
+            string outism = "";
+            foreach (var stud in Studs)
+            {
+                outism += (stud as Student).Surname + " ";
+                outism += (stud as Student).Name.Substring(0,1) + ".";
+                try
+                {
+                    outism += (stud as Student).Papa.Substring(0, 1) + "\n";
+                }
+                catch{}
+            }
+            return outism;
+        }
+
+        public static string coolGuys()
+        {
+            string outism = "";
+            foreach (var stud in Studs)
+            {
+                outism += (stud as Student).Surname + "; ";
+                outism += (stud as Student).Name + "; ";
+                outism += (stud as Student).Papa + "; Спец.: ";
+                outism += (stud as Student).Spec + "; Курс: ";
+                outism += (stud as Student).Course + "; ";
+                if ((stud as Student).Spec == "ИСиТ")
+                {
+                    outism += "Cool dude ( ͡° ͜ʖ ͡°)";
+                }
+                else
+                {
+                    outism += "Lalka kakaya-to";
+                }
+                
+                outism += (stud as Student).Course + "\n";
+            }
+            return outism;
+        }
+        
+        
+
         public static bool LessID(Student S1, Student S2)
         {
             return S1.ID <= S2.ID;
@@ -36,7 +95,7 @@ namespace laba14
 
         public static bool LessName(Student S1, Student S2)
         {
-            return string.Compare(S1.Name, S2.Name) <= 0;
+            return string.Compare(S1.Surname, S2.Surname) <= 0;
         }
         
         public static bool LessSpec(Student S1, Student S2)
@@ -78,10 +137,10 @@ namespace laba14
         
         static void Main()
         {
-            Studs.Add(new Student(1, "Петров А.С.", "МОиАИС", 2));
-            Studs.Add(new Student(2, "Иванов Д.И.", "ПИ", 1));
-            Studs.Add(new Student(3, "Агапова Л.В.", "МОиАИС", 3));
-            Studs.Add(new Student(4, "Закарян Ашот", "ИСТ", 4));
+            Studs.Add(new Student(1, "Александр", "МОиАИС", 2, "Петров", "Сергеевич"));
+            Studs.Add(new Student(2, "Дмитрий", "ПИ", 1, "Иванов", "Иванович"));
+            Studs.Add(new Student(3, "Людмила", "МОиАИС", 3, "Аагапова", "Валерьевна"));
+            Studs.Add(new Student(4, "Ашот", "ИСТ", 4, "Закарян", ""));
 
             PrintStuds("\nСписок студентов");
 
@@ -98,6 +157,18 @@ namespace laba14
 
             SortStud(LessID);
             PrintStuds("\nСортировка по номеру");
+            
+            PrintRule PR = new PrintRule(full);
+            
+            Console.WriteLine(PR());
+            
+            PR = new PrintRule(brief);
+            
+            Console.WriteLine(PR());
+            
+            PR = new PrintRule(coolGuys);
+            
+            Console.WriteLine(PR());
 
             Console.ReadLine();
         }
