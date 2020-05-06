78b38a4ceb1caa3aea29d2dac1b0e53f23281035
Mita57 <57thrasher57@gmail.com>
2020-05-07 00:22:43 +0500

    qwd

--- Year_2/ООП/semester2/la-bas15/ObserverPattern/ObserverPattern/Form2.cs
+++ Year_2/ООП/semester2/la-bas15/ObserverPattern/ObserverPattern/Form2.cs
@@ -1,12 +1,32 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace ObserverPattern
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        private Form1 mainf;
+        public Form2(Form f)
         {
             InitializeComponent();
+            mainf = (Form1) f;
+        }
+
+        public void action1()
+        {
+            Color c = BackColor;
+            BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(1000);
+            BackColor = c;
+            Refresh();
+            Thread.Sleep(500);
+        }
+
+        public void action2()
+        {
+            
         }
     }
 }
