cabf526e2468a7765e0d2cdb4abba36ef43406d1
Mita57 <57thrasher57@gmail.com>
2020-06-05 11:06:32 +0500

    yum

---
+++ Year_2/БИПИП/KR2/KR2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.partsNum = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.threadsNum = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.partsNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.threadsNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // partsNum
+            // 
+            this.partsNum.Location = new System.Drawing.Point(12, 40);
+            this.partsNum.Maximum = new decimal(new int[] {276447231, 23283, 0, 0});
+            this.partsNum.Name = "partsNum";
+            this.partsNum.Size = new System.Drawing.Size(120, 20);
+            this.partsNum.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнятор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // threadsNum
+            // 
+            this.threadsNum.Location = new System.Drawing.Point(149, 40);
+            this.threadsNum.Name = "threadsNum";
+            this.threadsNum.Size = new System.Drawing.Size(120, 20);
+            this.threadsNum.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Разбиения";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(149, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Потоки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 71);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.threadsNum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.partsNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.partsNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.threadsNum)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown partsNum;
+        private System.Windows.Forms.NumericUpDown threadsNum;
+
+        #endregion
+    }
+}
---
+++ Year_2/БИПИП/KR2/KR2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace KR2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double func(double arg)
+        {
+            return (arg * arg);
+        }
+
+        private static List<double> vals = new List<double>();
+        private static double littlePartition = 0;
+        private static int partition = 0;
+
+        private static double sum = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int partsAmount = (int) partsNum.Value;
+            int threadsAmount = (int) threadsNum.Value;
+            partition = partsAmount / threadsAmount;
+            littlePartition = (double) 2 / partsAmount;
+            for (double i = 0; i < 2; i += littlePartition)
+            {
+                vals.Add(func(i));
+            }
+            
+            for (int i = 0; i < partsAmount; i += partition)
+            {
+                Parallel.Invoke(() =>
+                {
+                    run(i + 1, i + partition);
+                });
+            }
+
+            MessageBox.Show(littlePartition.ToString());
+        }
+
+        private void run(int left, int right)
+        {
+            for (int i = left; i < right; i ++)
+            {
+                double val = ((vals[i - 1] + vals[i]) / (double) 2) * littlePartition;
+                sum += val;
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load("result.xml");
+                XmlElement xRoot = xDoc.DocumentElement;
+                XmlElement driverElem = xDoc.CreateElement("driver");
+                XmlElement driverCode = xDoc.CreateElement("arg");
+                XmlElement driverName = xDoc.CreateElement("val");
+                XmlText codeText = xDoc.CreateTextNode(i.ToString());
+                XmlText nameText = xDoc.CreateTextNode(val.ToString());
+                driverCode.AppendChild(codeText);
+                driverName.AppendChild(nameText);
+                driverElem.AppendChild(driverCode);
+                driverElem.AppendChild(driverName);
+                xRoot.AppendChild(driverElem);
+                xDoc.Save("result.xml");
+            }
+        }
+        
+    }
+}
