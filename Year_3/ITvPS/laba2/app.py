fb23f07893151c05dee1d2d85f5adad8a3de8b91
Mita57 <57thrasher57@gmail.com>
2020-10-24 21:07:35 +0500

    as

---
+++ Year_3/ITvPS/laba2/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template
+from pymystem3 import Mystem
+from collections import Counter
+
+app = Flask(__name__)
+
+
+class Thing:
+    def __init__(self, lemma, pos, count):
+        self.lemma = lemma
+        self.pos = pos
+        self.count = count
+
+
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    if request.method == 'POST':
+        return post_process(request)
+    else:
+        return render_template('Index.html')
+
+
+def post_process(request):
+    content = request.form['text']
+    m = Mystem()
+    counts = (dict(Counter(m.lemmatize(content))))
+    result = m.analyze(content)
+    stuff = []
+    for res in result:
+        try:
+            stuff.append(Thing(res['analysis'][0]['lex'], res['analysis'][0]['gr'].split('=')[0].split(',')[0],
+                               counts[res['analysis'][0]['lex']]))
+        except Exception:
+            pass
+    return render_template('Result.html', stuff=stuff)
+
+
+if __name__ == '__main__':
+    app.run()
