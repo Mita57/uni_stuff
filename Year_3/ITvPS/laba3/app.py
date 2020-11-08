4b941f5e5c974979eea25f86eb34d42e64598522
Mita57 <57thrasher57@gmail.com>
2020-11-09 01:16:21 +0500

    as

--- Year_3/ITvPS/laba3/app.py
+++ Year_3/ITvPS/laba3/app.py
@@ -31,47 +31,58 @@ def boolean(texts):
     m = Mystem()
     lemmas_by_text = {}
     lemmas = []
-    print(len(texts))
     for i in range(0, len(texts)):
         lemmas_by_text[i] = m.lemmatize(texts[i])
         lemmas.extend(m.lemmatize(texts[i]))
+    print('Lemmatizing finished')
 
     lemmas = list(dict.fromkeys(lemmas))
-    print(lemmas_by_text)
-    print(lemmas)
-    matrix = [[False for x in range(len(lemmas_by_text))] for y in range(len(lemmas))]
+    matrix = [[False for x in range(len(lemmas_by_text) - 1)] for y in range(len(lemmas))]
     for i in range(0, len(lemmas)):
-        for j in range(0, len(lemmas_by_text)):
-            print(j)
+        print('Text ' + str(i))
+        for j in range(0, len(lemmas_by_text) - 1):
             if lemmas[i] in lemmas_by_text[j]:
                 matrix[i][j] = True
             else:
                 pass
-    return render_template('boolean.html', matrix=matrix, lemmas=lemmas, length=(len(lemmas_by_text) - 1))
+    return render_template('boolean.html', matrix=matrix, lemmas=lemmas, length=(len(lemmas_by_text) - 1), title='Boolean')
 
 
 def term_frequency(texts):
     m = Mystem()
     lemmas_by_text = {}
     lemmas = []
-    print(len(texts))
     for i in range(0, len(texts)):
         lemmas_by_text[i] = m.lemmatize(texts[i])
         lemmas.extend(m.lemmatize(texts[i]))
+    print('Lemmatazing finsished')
 
     lemmas = list(dict.fromkeys(lemmas))
-    print(lemmas_by_text)
-    print(lemmas)
-    matrix = [[False for x in range(len(lemmas_by_text))] for y in range(len(lemmas))]
+    matrix = [[0 for x in range(len(lemmas_by_text) - 1)] for y in range(len(lemmas))]
     for i in range(0, len(lemmas)):
-        for j in range(0, len(lemmas_by_text)):
-            print(j)
+        for j in range(0, len(lemmas_by_text) - 1):
             matrix[i][j] = lemmas_by_text[j].count(lemmas[i])
-    return render_template('boolean.html', matrix=matrix, lemmas=lemmas, length=(len(lemmas_by_text) - 1))
+    return render_template('boolean.html', matrix=matrix, lemmas=lemmas, length=(len(lemmas_by_text) - 1), title='Term frequency')
 
 
 def tf_idf(texts):
-    pass
+    m = Mystem()
+    lemmas_by_text = {}
+    lemmas = []
+    for i in range(0, len(texts)):
+        lemmas_by_text[i] = m.lemmatize(texts[i])
+        lemmas.extend(m.lemmatize(texts[i]))
+    print('Lemmatazing finsished')
+
+    lemmas = list(dict.fromkeys(lemmas))
+    matrix = [[0 for x in range(len(lemmas_by_text) - 1)] for y in range(len(lemmas))]
+    for i in range(0, len(lemmas)):
+        for j in range(0, len(lemmas_by_text) - 1):
+            try:
+                matrix[i][j] = lemmas_by_text[j].count(lemmas[i]) / len(lemmas_by_text[j])
+            except:
+                matrix[i][j] = lemmas_by_text[j].count(lemmas[i]) / 1
+    return render_template('boolean.html', matrix=matrix, lemmas=lemmas, length=(len(lemmas_by_text) - 1), title='tf_idf')
 
 
 if __name__ == '__main__':
