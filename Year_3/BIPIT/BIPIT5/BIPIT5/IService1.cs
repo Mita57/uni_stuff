513e32fe0af1ad5458bbc64e7beec84b3e74ce45
Mita57 <57thrasher57@gmail.com>
2020-11-18 23:48:05 +0500

    soos

--- Year_3/BIPIT/BIPIT5/BIPIT5/IService1.cs
+++ Year_3/BIPIT/BIPIT5/BIPIT5/IService1.cs
@@ -12,34 +12,13 @@ namespace BIPIT5
     public interface IService1
     {
         [OperationContract]
-        string GetData(int value);
+        List<List<string>> GetData(string table);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        void NewRec(List<string> vals, List<string> cols, string table);
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "BIPIT5.ContractType".
     [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+   
 }
--- Year_3/BIPIT/BIPIT5/BIPIT5/Service1.cs
+++ Year_3/BIPIT/BIPIT5/BIPIT5/Service1.cs
@@ -4,28 +4,73 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace BIPIT5
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+
+        static SqlConnection _connection;
+
+        public void NewRec(List<string> vals, List<string> cols,string table)
         {
-            return string.Format("You entered: {0}", value);
+            String connectString = @"Data Source = EquipmentFFSVer4.mssql.somee.com ; Initial Catalog = EquipmentFFSVer4;User ID = fckinslayer_SQLLogin_1; password = dtv5jblqyo; MultipleActiveResultSets=true";
+            _connection = new SqlConnection(connectString);
+            _connection.Open();
+
+            String newValues = "";
+            foreach (String str in vals)
+            {
+                newValues += "'" + str + "', ";
+            }
+            newValues = newValues.Substring(0, newValues.Length - 2);
+
+
+            string newCols = "";
+
+            foreach (string str in cols)
+            {
+                newCols += "" + str + ", ";
+            }
+
+            newCols = newCols.Substring(0, newCols.Length - 2);
+
+
+            String query = String.Format("INSERT INTO {0}({1}) VALUES ({2})", table, newCols, newValues);
+
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.InsertCommand = command;
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            _connection.Close();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        List<List<string>> IService1.GetData(string table)
         {
-            if (composite == null)
+            String connectString = @"Data Source = EquipmentFFSVer4.mssql.somee.com ; Initial Catalog = EquipmentFFSVer4;User ID = fckinslayer_SQLLogin_1; password = dtv5jblqyo; MultipleActiveResultSets=true";
+            _connection = new SqlConnection(connectString);
+            _connection.Open();
+            String query = String.Format("SELECT * FROM {0}", table);
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+            List<List<string>> listGlobal = new List<List<string>>();
+            List<string> listLocal = new List<string>();
+            while (dataReader.Read())
             {
-                throw new ArgumentNullException("composite");
+                listLocal.Add(dataReader.GetValue(0).ToString());
+                listLocal.Add(dataReader.GetValue(1).ToString());
+                listLocal.Add(dataReader.GetValue(2).ToString());
+                if(table == "Issues")
+                {
+                    listLocal.Add(dataReader.GetValue(3).ToString());
+                    listLocal.Add(dataReader.GetValue(4).ToString());
+                }
+                listGlobal.Add(listLocal);
             }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return listGlobal;
         }
     }
 }
