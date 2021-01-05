ce04f0ed23efde38c62cbf56d5a556a0a5de8f5b
Mita57 <57thrasher57@gmail.com>
2021-01-05 22:51:14 +0500

    wd

---
+++ Year_3/BIPIT/BIPIT9/BIPIT9/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BIPIT9.Models;
+
+namespace BIPIT9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect(Url.Action("Issues", "Home"));
+        }
+
+        public ActionResult Issues()
+        {
+            using (EquipmentFFSEntities1 db = new EquipmentFFSEntities1())
+            {
+                ViewBag.Issues = db.Issues.ToList();
+                return View();
+            }
+        }
+
+        public ActionResult Employees()
+        {
+            using (EquipmentFFSEntities1 db = new EquipmentFFSEntities1())
+            {
+                ViewBag.Employees = db.Employees.ToList();
+                return View();
+            }
+        }
+
+        public ActionResult Equipment()
+        {
+            using (EquipmentFFSEntities1 db = new EquipmentFFSEntities1())
+            {
+                ViewBag.Equipment = db.Equipments.ToList();
+                return View();
+            }
+        }
+
+    }
+}
