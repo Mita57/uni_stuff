b91c4e9ab2f80353cbcbeee2ce448fdde0e5ab7f
Mita57 <57thrasher57@gmail.com>
2021-01-10 01:21:10 +0500

    NAVI DAVI

---
+++ Year_3/BIPIT/BIPIT10/BIPIT10/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BIPIT10.Models;
+
+namespace BIPIT9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect(Url.Action("Issues", "Home"));
+        }
+
+        public ActionResult Issues()
+        {
+            using (EquipmentFFSEntities db = new EquipmentFFSEntities())
+            {
+                ViewBag.Issues = db.Issues.ToList();
+                return View();
+            }
+        }
+
+        public ActionResult Employees()
+        {
+            using (EquipmentFFSEntities db = new EquipmentFFSEntities())
+            {
+                ViewBag.Employees = db.Employees.ToList();
+                return View();
+            }
+        }
+
+        public ActionResult Equipment()
+        {
+            using (EquipmentFFSEntities db = new EquipmentFFSEntities())
+            {
+                ViewBag.Equipment = db.Equipments.ToList();
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Year_3/BIPIT/BIPIT10/BIPIT10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BIPIT10.Startup))]
+namespace BIPIT10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
