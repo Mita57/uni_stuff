ae4922c697fef4db3094680fd7be2c02fc5e93fd
Mita57 <57thrasher57@gmail.com>
2021-01-17 16:30:02 +0500

    asd

---
+++ Year_3/BIPIT/laba11/laba11/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace laba11.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
