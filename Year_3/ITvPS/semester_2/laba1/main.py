52380e2f900b9fe188ee11f0a153ec465f3f3872
Mita57 <57thrasher57@gmail.com>
2021-03-26 14:07:48 +0500

    yee

---
+++ Year_3/ITvPS/semester_2/laba1/main.py
@@ -0,0 +1,11 @@
+def get_ending(amount):
+    if int(amount[-1]) == 0 or int(amount[-1]) > 4:
+        return number + ' рублей'
+    if int(amount[-1]) == 1:
+        return number + ' рубль'
+    return number + ' рубля'
+
+
+while True:
+    number = input('Entrez votre numéro')
+    print(get_ending(number))
